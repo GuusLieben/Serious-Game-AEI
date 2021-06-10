ecc652633bf9ad0817d306c6fc8fb779a15c2daa
Rade <r.grbic@student.avans.nl>
2021-06-10 15:41:33 +0200

    Added TeamController

---
+++ Assets/Scripts/NewTeamController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NewTeamController : MonoBehaviour
+{
+    [SerializeField] 
+    private TeamController teamController;
+
+    public void StartGame()
+    {
+        StartCoroutine(PostTeam());
+    }
+
+    IEnumerator PostTeam()
+    {
+        string teamName = this.teamController.GetTeamName();
+        List<string> players = this.teamController.GetPlayers();
+
+        Team team = new Team()
+        {
+            teamName = teamName,
+            playerNames = players
+        };
+
+        string teamString = 
+
+        using (UnityWebRequest addTeamRequest =
+            UnityWebRequest.Post("https://avans-schalm-appserver.azurewebsites.net/api/game/join?gameCode=" + this.teamController.GetGroupCode(), teamString))
+        {
+            yield return addTeamRequest.SendWebRequest();
+            Debug.Log("TeamResult: " + addTeamRequest.result);
+
+            if (addTeamRequest.result != UnityWebRequest.Result.Success)
+            {
+                Debug.Log(addTeamRequest.error);
+            }
+            else
+            {
+                Debug.Log("Team Added!");
+                
+            }
+        }
+    }
+}
